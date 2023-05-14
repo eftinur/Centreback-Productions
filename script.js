86cf70123b819d850946a7f3cc273f58e7bca024
eftinur <eftinur90@gmail.com>
2023-05-14 19:41:27 +0600

    fixed z-index Error in the Hero section

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 const images = document.getElementsByClassName("image");
 
-let globalIndex = 0;
-console.log(globalIndex);
-let last = { x: 0, y: 0 };
+let globalIndex = 0,
+    last = { x: 0, y: 0 };
 
-const activate = (images, x, y) => {
-  images.style.left = `${x}px`;
-  images.style.top = `${y}px`;
+const activate = (image, x, y) => {
+  image.style.left = `${x}px`;
+  image.style.top = `${y}px`;
+  image.style.zIndex = globalIndex;
 
-  images.dataset.status = "active";
+  image.dataset.status = "active";
 
   last = { x, y };
-};
+}
 
 const distanceFromLast = (x, y) => {
   return Math.hypot(x - last.x, y - last.y);
-};
+}
 
-window.onmousemove = (e) => {
-  if (distanceFromLast(e.clientX, e.clientY) > 100) {
-    const lead = images[globalIndex % images.length];
-    const tail = images[(globalIndex - 5) % images.length];
+const handleOnMove = e => {
+  if(distanceFromLast(e.clientX, e.clientY) > (window.innerWidth / 20)) {
+    const lead = images[globalIndex % images.length],
+          tail = images[(globalIndex - 5) % images.length];
 
     activate(lead, e.clientX, e.clientY);
 
-    if (tail) tail.dataset.status = "inactive";
-
+    if(tail) tail.dataset.status = "inactive";
+    
     globalIndex++;
   }
-};
+}
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
