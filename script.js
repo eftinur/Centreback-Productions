e1a5f90ae37fedb15d22bc0055636509bb873fe4
eftinur <eftinur90@gmail.com>
2023-06-15 16:47:15 +0600

    added custom Mouse Cursor Trail

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+// Mouse Cursor Trail
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    "top: " + (e.pageY - 15) + "px; left: " + (e.pageX - 15) + "px;"
+  );
+});
+
+document.addEventListener("click", () => {
+  cursor.classList.add("expand");
+
+  setTimeout(() => {
+    cursor.classList.remove("expand");
+  }, 500);
+});
+
+// Lenis Setup
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
 
 // Hero Area Image Trail Animation
 const images = document.getElementsByClassName("image");
@@ -36,7 +66,7 @@ window.onmousemove = (e) => handleOnMove(e);
 
 window.ontouchmove = (e) => handleOnMove(e.touches[0]);
 
-// GreenSock Animation
+// GreenSock
 gsap.from(
   ".test",
   {
@@ -52,34 +82,32 @@ gsap.from(
   0
 );
 
-const { innerHeight } = window;
+// GSAP Navigation
+const revealBtn = document.querySelector(".hamburger_menu");
+const unrevealBtn = document.querySelector(".unreveal_btn");
 
-// Zoom Out on Scrolling
-gsap.from(".zoom_out h2", {
-  scale: 50,
-  opacity: 0,
-  stagger: 0.25,
-  duration: 3,
-  scrollTrigger: {
-    trigger: ".zoom_out",
-    pin: true,
-    end: `+=${innerHeight * 1.3}`,
-    scrub: 3,
-    markers: true,
-  },
+let menuTl = gsap.timeline({ paused: true });
+menuTl
+  .to(".menu_main", {
+    opacity: 1,
+    duration: 1,
+    top: 0,
+    ease: Power2.easeInOut,
+  })
+  .from(".nav_list", {
+    opacity: 0,
+    y: 20,
+    duration: 1,
+    ease: Power2.easeInOut,
+    stagger: 0.3,
+  });
+
+revealBtn.addEventListener("click", () => {
+  menuTl.play().timeScale(1);
 });
 
-// Zoom In on Scrolling
-gsap.to(".zoom_in h2", {
-  scale: 50,
-  opacity: 0,
-  stagger: 0.25,
-  duration: 3,
-  scrollTrigger: {
-    trigger: ".zoom_in",
-    pin: true,
-    end: `+=${innerHeight * 1.3}`,
-    scrub: 3,
-    markers: true,
-  },
+unrevealBtn.addEventListener("click", () => {
+  menuTl.timeScale(2.5);
+  menuTl.reverse();
+  console.log("Hola");
 });
